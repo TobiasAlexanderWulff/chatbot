02c0da9721ca1ca3396fe16fddee9c5e6882da96
Tobias Alexander Wulff <tobias.wulff@live.de>
2025-02-16 13:16:39 +0100

    added Voice class

--- src/voice.py
+++ src/voice.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import torch
 from TTS.api import TTS
 import pyaudio
@@ -13,60 +12,63 @@ speaker = "tts_models/de/thorsten/tacotron2-DDC"
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-try:
-    tts = TTS(speaker).to(device)
-except FileNotFoundError:
-    print("Speaker not found")
-    exit(1)
+
+
+class Voice:
+    def __init__(self):
+        try:
+            self.tts = TTS(speaker).to(device)
+        except FileNotFoundError:
+            print("Speaker not found")
+            exit(1)
+        
+        self.p = pyaudio.PyAudio()
+        
+        if not os.path.exists("temp"):
+            os.makedirs("temp")
     
-p = pyaudio.PyAudio()
+    def synthesize(self, text):
+        target_path = f"temp/synth.wav"
+        self.tts.tts_to_file(text=text, file_path=target_path)
+        return target_path
     
-if not os.path.exists("temp"):
-        os.makedirs("temp")
-
-
-def synthesize(text):
-    print("Synthesizing...")
-    target_path = f"temp/synth.wav"
-    tts.tts_to_file(text=text, file_path=target_path)
-    print("Synthesized")
-    return target_path
-
-
-def play(file_path):
-    chunk = 1024
-
-    try:
-        wf = wave.open(file_path, 'rb')
-    except FileNotFoundError:
-        print("File not found")
-        exit(1)
-
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True,
-                    output_device_index=0)
-
-    data = wf.readframes(chunk)
-
-    while data:
-        stream.write(data)
+    def play(self, file_path):
+        chunk = 1024
+        try:
+            wf = wave.open(file_path, 'rb')
+        except FileNotFoundError:
+            print("File not found")
+            exit(1)
+    
+        stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True,
+                        output_device_index=0)
+    
         data = wf.readframes(chunk)
+    
+        while data:
+            stream.write(data)
+            data = wf.readframes(chunk)
+    
+        stream.stop_stream()
+        stream.close()
 
-    stream.stop_stream()
-    stream.close()
+    
+    def speak(self, string):
+        print("Speaking...")
+        print(string)
+        self.play(self.synthesize(string))
+        print("Finished speaking")
+    
+    def close(self):
+        self.p.terminate()
 
-    p.terminate()
-
-
-def speak(string):
-    print("Speaking...")
-    print(string)
-    play(synthesize(string))
-    print("Finished speaking")
 
 
 if __name__ == "__main__":  
-    speak(string=test_text_de)
+    voice = Voice()
+    voice.speak(test_text_de)
+    voice.close()
     
